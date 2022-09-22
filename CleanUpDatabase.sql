80cb391deaef887e79772926d529a8be1a67eb68
maveriCkharsha <harsha.vesangi@gmail.com>
2022-09-22 17:25:06 +0530

    Update CleanUpDatabase.sql

--- CleanUpDatabase.sql
+++ CleanUpDatabase.sql
@@ -28,7 +28,28 @@ SELECT relname,
    pg_stat_user_tables
  ORDER BY
    n_live_tup DESC;
+   
+   
+-- List of Duplicate indexes
 
+SELECT   indrelid::regclass table_name,
+         att.attname column_name,
+         amname index_method
+FROM     pg_index i,
+         pg_class c,
+         pg_opclass o,
+         pg_am a,
+         pg_attribute att
+WHERE    o.oid = ALL (indclass) 
+AND      att.attnum = ANY(i.indkey)
+AND      a.oid = o.opcmethod
+AND      att.attrelid = c.oid
+AND      c.oid = i.indrelid
+GROUP BY table_name, 
+         att.attname,
+         indclass,
+         amname, indkey
+HAVING count(*) > 1;
 
 -- #3. Cleaning up unused indexes
 
